57ca7fc641423be4ca563f76354b9f2efe299f6f
NowNewStart <j.gelmar@hotmail.de>
2017-05-17 22:42:41 +0200

    added a few filters and removed cod-like color codes

--- client/components/Serverlist.vue
+++ client/components/Serverlist.vue
@@ -1,5 +1,13 @@
 <template>
   <p class="serverlist">
+    <span class="columns">
+      <span class="column" style="margin-left: 10px">
+        <span class="block">
+          <a class="button is-primary" @click="filterOutRP">Filter out RP servers</a>
+          <a class="button is-primary" @click="filterOutFrenchies">Filter out French servers</a>
+        </span>
+      </span>
+    </span>
     <table class="table">
       <thead>
         <tr>
@@ -11,7 +19,7 @@
       </thead>
       <tbody>
         <tr v-for="server in serverdata">
-          <td>{{ server.Data.hostname }}</td>
+          <td>{{ removeSpecialCharacters(server.Data.hostname) }}</td>
           <td>{{ server.Data.clients }}</td>
           <td>{{ server.Data.svMaxclients }}</td>
           <td><a :href="'fivem://connect/' + server.EndPoint">{{ server.EndPoint }}</a></td>
@@ -38,6 +46,8 @@ export default {
           if(a.Data.hostname > b.Data.hostname) return 1
           return 0
         })
+      }).catch(function (error) {
+        console.log(error)
       })
     },
     reloadServerlistByPlayers: function() {
@@ -48,7 +58,33 @@ export default {
           if(a.Data.players.length > b.Data.players.length) return -1
           return 0
         })
+      }).catch(function (error) {
+        console.log(error)
       })
+    },
+    filterOutRP: function() {
+      this.serverdata = this.serverdata.filter(function (server) {
+        var hostname = server.Data.hostname.toLowerCase();
+        return (hostname.indexOf('rp') === -1 && hostname.indexOf('roleplay') === -1 && hostname.indexOf('role play') === -1)
+      })
+    },
+    filterOutFrenchies: function() {
+      this.serverdata = this.serverdata.filter(function (server) {
+        var hostname = server.Data.hostname.toLowerCase();
+        return (hostname.indexOf('fr') === -1 && hostname.indexOf('french') === -1 && hostname.indexOf('france') === -1)
+      })
+    },
+    removeSpecialCharacters: function(hostname) {
+      return hostname.replace(/\^1/g,"")
+                                    .replace(/\^2/g,"")
+                                    .replace(/\^3/g,"")
+                                    .replace(/\^4/g,"")
+                                    .replace(/\^5/g,"")
+                                    .replace(/\^6/g,"")
+                                    .replace(/\^7/g,"")
+                                    .replace(/\^8/g,"")
+                                    .replace(/\^9/g,"")
+                                    .replace(/\^0/g,"")
     }
   },
   mounted () {
